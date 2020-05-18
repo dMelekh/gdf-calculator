e7b055d8378505231e6de31bb4c6b5156db6fdbc
Dmitry Melekh <melekh.dm@gmail.com>
2020-05-18 18:04:52 +0300

    added GasStateByPi, GasStateByTau, GasStateByEpsilon

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ class GasStateByMach {
         this.kappa = 1.4; // kappa = 1.4 for normal air
         this.mach = 1.0;
     }
-    
+
     setKappa(kappa) {
         this.kappa = +kappa;
     }
-    
+
     getKappa() {
         return this.kappa;
     }
-    
+
     setMach(mach) {
         this.mach = +mach;
-    }   
+    }
 
     getMach() {
         return this.mach;
@@ -63,7 +63,7 @@ class GasStateByMach {
 }
 
 class GasStateByLambda {
-    
+
     constructor() {
         this.kappa = 1.4; // kappa = 1.4 for normal air
         this.lambda = 1.0;
@@ -72,7 +72,7 @@ class GasStateByLambda {
     setKappa(kappa) {
         this.kappa = +kappa;
     }
-    
+
     getKappa() {
         return this.kappa;
     }
@@ -80,15 +80,15 @@ class GasStateByLambda {
     setLambda(lambda) {
         this.lambda = +lambda;
     }
-    
+
     getLambda() {
         return this.lambda;
     }
-    
+
     getMach() {
         return Math.sqrt(2.0 * Math.pow(this.getLambda(), 2.0) / (this.kappa + 1.0 - Math.pow(this.getLambda(), 2.0) * (this.kappa - 1.0)));
     }
-    
+
     getPi() {
         return Math.pow(this.getEpsilon(), this.kappa);
     }
@@ -170,7 +170,7 @@ class GasStateByQu extends GasStateDelegate {
     constructor() {
         super(new GasStateByLambda());
         this.qu = this.getQu();
-        this.funQuByLambda = function(lambda) {
+        this.funQuByLambda = function (lambda) {
             this.setLambda(lambda);
             return this.getQu();
         };
@@ -195,8 +195,63 @@ class GasStateByQu extends GasStateDelegate {
             getArgByHalfMethod(this.funQuByLambda, this.qu, this.leftLyambda, this.rightLyambda)
         );
     }
+
+    getQu() {
+        return this.qu;
+    }
 }
 
+class GasStateByPi extends GasStateDelegate {
+    constructor() {
+        super(new GasStateByMach());
+    }
+
+    setPi(pi) {
+        this.pi = +pi;
+        this.wrappedState.setMach(
+            Math.sqrt((Math.pow(1.0 / this.pi, (this.getKappa() - 1.0) / this.getKappa()) - 1.0) * 2.0 / (this.getKappa() - 1.0))
+        );
+    }
+
+    getPi() {
+        return this.pi;
+    }
+}
+
+class GasStateByTau  extends GasStateDelegate {
+    constructor() {
+        super(new GasStateByMach());
+    }
+
+    setTau(tau) {
+        this.tau = +tau;
+        this.wrappedState.setMach(
+            Math.sqrt((1.0 / this.tau - 1.0) * 2.0 / (this.getKappa() - 1.0))
+        );
+    }
+
+    getTau() {
+        return this.tau;
+    }
+
+}
+
+class GasStateByEpsilon extends GasStateDelegate {
+    constructor() {
+        super(new GasStateByMach());
+    }
+    
+    setEpsilon(epsilon) {
+        this.epsilon = +epsilon;
+        this.wrappedState.setMach(
+            Math.sqrt((Math.pow(1.0 / this.epsilon, this.getKappa()-1.0)-1.0)*2.0/(this.getKappa()-1.0))
+        );
+    }
+    
+    getEpsilon() {
+        return this.epsilon;
+    }
+}
 
 function updateFields(fields, state) {
     fields['kappa'].value = state.getKappa();
@@ -219,17 +274,24 @@ let inputsByNames = splitElementsByName(inputs);
 //console.log('stateByLambda');
 //updateFields(inputsByNames, stateByLambda);
 
-let stateByQu = new GasStateByQu();
-console.log('stateByQu');
+//let stateByQu = new GasStateByQu();
+//console.log('stateByQu');
+//stateByQu.setQu(0.5);
+//updateFields(inputsByNames, stateByQu);
 
-console.log(stateByQu.wrappedState.getLambda());
-console.log(stateByQu.getLambda());
+//let stateByPi = new GasStateByPi();
+//console.log('stateByPi');
+//stateByPi.setPi(0.5);
+//updateFields(inputsByNames, stateByPi);
 
-stateByQu.setQu(0.5);
+//let stateByTau = new GasStateByTau();
+//console.log('stateByTau');
+//stateByTau.setTau(0.5);
+//updateFields(inputsByNames, stateByTau);
 
-console.log(stateByQu.wrappedState.getLambda());
-console.log(stateByQu.getLambda());
-console.log(stateByQu.wrappedState.getKappa());
-console.log(stateByQu.getKappa());
+let stateByEpsilon = new GasStateByEpsilon();
+console.log('stateByTau');
+stateByEpsilon.setEpsilon(0.5);
+updateFields(inputsByNames, stateByEpsilon);
 
-updateFields(inputsByNames, stateByQu);
+//updateFields(inputsByNames, stateByQu);
